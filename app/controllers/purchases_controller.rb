6129c3a852928fd119a19cf9a8f7100025b23fe7
Lara Aydin <ayd.dilara@gmail.com>
2017-06-23 17:08:54 -0400

    Add Stripe chceckout

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,19 @@
 class PurchasesController < ApplicationController
   def home
+    @products = Product.order(:price)
   end
 
   def create
+    Stripe.api_key = "sk_test_JYIRyATUyu3u9Cwj1D7R9gYZ"
+
+    token = params[:stripeToken]
+    product = Product.find(params[:product_id])
+
+    charge = Stripe::Charge.create(
+      amount: product.price,
+      currency: "usd",
+      description: "#{product.name} - #{product.kiss_count} llama kisses",
+      source: token,
+    )
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def format_currency(price_in_cents)
+    number_to_currency(price_in_cents / 100)
+  end
 end
